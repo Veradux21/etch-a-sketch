c562eef2e55883c821e689e9bd46b4af0d3f48e4
Veradux21 <Veradux4@protonmail.com>
2022-06-22 18:00:51 -0400

    Added Grid Creation Function and Mouseover Event
    Created a function to update the grid with a loop based on a provided variable
    Added css with transition assigned to each cell that transitions on mouseover
    
    Still need to fix the flexboxes and add the ability to accept user submitted gridSize

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-let gridSize = 0;
+let gridSize = 16;
+
+const button = document.querySelector('button')
+button.addEventListener('click', () =>{
+    updateGrid();
+});
+
+
+
+function updateGrid () {
+    const body = document.querySelector('body')
+    const gridHome = document.createElement('div')
+    body.appendChild(gridHome)
+    for (let i = gridSize; i > 0 ; i-- ){
+        const collumn = document.createElement('div')
+        collumn.classList.add('etch')
+        collumn.addEventListener('mouseover', () => {
+            collumn.classList.remove('etch');
+            collumn.classList.add('sketch');
+        });
+        for (let j = gridSize; j > 0; j--){
+            const row = document.createElement('div')
+            row.classList.add('etch')
+            row.addEventListener('mouseover', () => {
+                row.classList.remove('etch');
+                row.classList.add('sketch');
+            });
+            collumn.appendChild(row);
+        };
+        gridHome.appendChild(collumn)
+    };
+    gridSize = 0
+};
