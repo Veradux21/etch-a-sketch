c8312e7adf093171579807f39809e5a31069c255
Veradux21 <Veradux4@protonmail.com>
2022-06-23 10:43:01 -0400

    Add prompt and fixed cell sizing
    Cells now displace in a static sized box.
    Cells now respond with a color change to mouseover and, after a delay, returns to old color

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
-let gridSize = 16;
+let gridSize = 0;
 
 const button = document.querySelector('button')
 button.addEventListener('click', () =>{
-    updateGrid();
+    gridSize = prompt("Size grid");
+    if(gridSize > 100){alert("Input a number less than 100!")}
+    else{
+            const body = document.querySelector('body');
+            const oldGrid = document.querySelector('.gridHome');
+            if(oldGrid !== null){body.removeChild(oldGrid)};
+            updateGrid();
+        };
 });
 
 
 
 function updateGrid () {
-    const body = document.querySelector('body')
-    const gridHome = document.createElement('div')
+    const body = document.querySelector('body');
+    const gridHome = document.createElement('div');
+    gridHome.classList.add('gridHome');
     body.appendChild(gridHome)
     for (let i = gridSize; i > 0 ; i-- ){
         const collumn = document.createElement('div')
-        collumn.classList.add('etch')
+        collumn.classList.add('etch');
         collumn.addEventListener('mouseover', () => {
-            collumn.classList.remove('etch');
             collumn.classList.add('sketch');
+        collumn.addEventListener('mouseout', () =>{
+            collumn.classList.remove('sketch');
+        });
         });
         for (let j = gridSize; j > 0; j--){
-            const row = document.createElement('div')
-            row.classList.add('etch')
+            const row = document.createElement('div');
+            row.classList.add('etch');
             row.addEventListener('mouseover', () => {
-                row.classList.remove('etch');
                 row.classList.add('sketch');
+            });
+            row.addEventListener('mouseout', () =>{
+                row.classList.remove('sketch');
             });
             collumn.appendChild(row);
         };
-        gridHome.appendChild(collumn)
+        gridHome.appendChild(collumn);
     };
     gridSize = 0
 };
